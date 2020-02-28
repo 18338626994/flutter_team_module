de4ec397b105ab20df83634c8dccc0d2c07dc756
yunfei <861860170@qq.com>
2020-02-28 17:32:52 +0800

    flutter_module init

---
+++ .ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/huifu_yuyunfei/开发/flutter_1.9.4/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/huifu_yuyunfei/Desktop/Flutter/flutter-modules/flutter_team_module"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
